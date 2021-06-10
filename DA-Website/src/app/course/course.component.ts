414435616717a060b20c846c8034d1437781fe27
academy_intern_mac05 <academy_intern_mac05@academy-intern-mac05s-MacBook-Air.local>
2021-06-10 16:53:21 +0200

    course object

--- DA-Website/src/app/course/course.component.ts
+++ DA-Website/src/app/course/course.component.ts
@@ -11,5 +11,11 @@ export class CourseComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  course = [
+    { 'id': 1, 'name': 'Learn Angular', 'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.','image':'../../assests/Angular.png' },
+    { 'id': 2, 'name': 'Learn Typerscript', 'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.','image':'../../assests/Typescript.png' },
+    { 'id': 3, 'name': 'Learn Nodejs', 'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 'image':'../../assests/nodejs.jpeg' },
+    { 'id': 4, 'name': 'Learn JavaScript', 'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.','image':'../../assests/Javascript.png' }
+  ]
 
 }
